7e15cfa5c480e52e01c9838e9bd33ae3c552ab57
LuisSteel07 <luisdelfonseca220@gmail.com>
2025-11-03 09:23:02 -0500

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.addingusersproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.addingusersproject to javafx.fxml;
+    exports com.example.addingusersproject;
+}
